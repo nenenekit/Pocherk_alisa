29ae9ee983e15f4fe91161e7a7a4679017afeaf7
N_OFFICE_4 <nikitatohonov432@mail.ru>
2025-07-27 22:42:15 +0300

    fufu

---
+++ komp/день 3.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+счетчик=5
+while счетчик > 0:
+    print(счетчик)
+    счетчик -= 1
+игры =['Minecraft','Roblox']
+игры.append('Among Us')
+игры.remove('Roblox')
+print(игры[0])# Первый элемент списка
+for игра  in игры:
+    print(игра)
